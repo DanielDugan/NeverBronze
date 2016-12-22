e02c80682b5b5e2a35844a6cdde196c55713cbd3
Edward <egates01@baker.edu>
2016-12-21 20:38:32 -0500

    New Solution

---
+++ NeverBronze/NeverBronze/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NeverBronze.Startup))]
+namespace NeverBronze
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
