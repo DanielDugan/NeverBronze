35fafe113af332106a7741b404312a8d6117dd5d
kchirayath <kevinchirayath@gmail.com>
2017-01-16 18:48:16 -0500

    D3 Timeline chart example

---
+++ NeverBronze/NeverBronze/Scripts/D3/TimeLIne.js
@@ -0,0 +1,16 @@
+﻿//example Timeline chart
+var testData = [
+  {
+      class: "pA", label: "Player 1", times: [
+       { "starting_time": 1355752800000, "ending_time": 1355759900000 },
+       { "starting_time": 1355767900000, "ending_time": 1355774400000 }]
+  },
+  {
+      class: "pB", label: "Player 2", times: [
+       { "starting_time": 1355759910000, "ending_time": 1355761900000 }]
+  },
+  {
+      class: "pC", label: "Players", times: [
+       { "starting_time": 1355761910000, "ending_time": 1355763910000 }]
+  }
+];
